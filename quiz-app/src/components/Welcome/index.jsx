105eb366804dcd6e731bac64b80c8f3604ca733a
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-12-06 11:14:09 -0300

    create state context

--- quiz-app/src/components/Welcome/index.jsx
+++ quiz-app/src/components/Welcome/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import imageQuiz from '../../img/quiz.svg'
 import "./style.css"
+import { useContext } from 'react'
+import { QuizContext } from '../../context/quiz'
 
 export const Welcome = () => {
+  const quizState = useContext(QuizContext)
+  console.log(quizState);
   return (
     <div id='Welcome'>
         <h2>Seja Bem-Vindo!</h2>
